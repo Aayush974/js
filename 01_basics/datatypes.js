71ebca24a5d3cbddd48f5a70e0cec9f758423f1a
Aayush974 <chandayush005@gmail.com>
2024-06-21 09:20:10 +0000

    delete

--- 01_basics/datatypes.js
+++
@@ -1,3 +0,0 @@
-"use strict"; //teates all JS code as newer version
-/* the official documentation for js standards is ECMA script
-    mdn is a readable version  */
--- 01_basics/variable.js
+++
@@ -1,6 +0,0 @@
-const accId=134 // const declares a variable which cannot be changed i.e a constant so to speak
-let name='xyz' // let values can be changed and reassigned down the line 
-var amount=4000  // old method of declaaring a variable causes issues with block scope and functional scope
-maturityDate='1/12/23' // variables can also be defined like this tho its a shitty method to do so
-let roi; // this is an undefined variable its value can be set later
-console.table([accId,name,amount,maturityDate,roi])
