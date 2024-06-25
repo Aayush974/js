4856004cce85bc283a7c68807dc9a11e7c9aae1c
Aayush974 <chandayush005@gmail.com>
2024-06-25 13:22:07 +0000

    commit

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,11 @@
+// ways to write a string: can be written in either "" or '' pr using backticks `` known as templates string
+
+// backticks make code more readable variables can be used directly using placeholders for them
+
+const name="user"
+console.log(`this is a greeting from the ${name}`)
+
+// another way to declare a string is using-
+
+const gameName= new String('mario')
+
