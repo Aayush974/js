105f2f52728e06c7a05d05ae7faf5689b4230ff1
Aayush974 <chandayush005@gmail.com>
2024-06-30 05:46:23 +0000

    dateAndTime

---
+++ 01_basics/07_dateAndTime.js
@@ -0,0 +1,12 @@
+const date = new Date()
+// console.log(date);
+// console.log(date.toDateString());
+// console.log(date.toISOString());
+// console.log(date.toLocaleTimeString());
+// console.log(date.toLocaleString());
+
+const createdDate=new Date(2023,0,23,6,10,5,500) // year,month,date,hours,minutes,seconds,miliseconds
+
+console.log(createdDate);
+
+console.log(date);
