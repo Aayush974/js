106a408382aab65c1a221991d9d45efd56ae2e6c
Aayush974 <chandayush005@gmail.com>
2024-07-02 05:13:30 +0000

    arrays

---
+++ 02_basics/01_arrays.js
@@ -0,0 +1,17 @@
+const arr=[1,2,3,4,5]
+//arr[0] //arrays are zero indexed, this accesses the first elemnt of the array
+
+//array methods
+
+
+// arr.push(6); // pushes the given argument to the last of the array
+
+// arr.pop();  // removes the last item form the array
+
+
+// arr.unshift(8); // inserts element at the start of the array
+
+// arr.shift(); //Removes the first element from an array and returns it. If the array is empty, undefined is returned and the array is not modified.
+
+console.log( arr.includes(3)); //determines wheter the array contains an element or not and returns corresponding boolean value
+console.log(arr.indexOf(18));//returns the index of the first occurence of the given value,returns -1 if the value is not present in the array
