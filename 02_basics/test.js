e3aa84c0f613426708a00169a1a97bfba05b0ca9
Aayush974 <chandayush005@gmail.com>
2024-07-25 13:18:01 +0000

    classes

---
+++ 02_basics/test.js
@@ -0,0 +1,27 @@
+// let protoRabbit = {
+//     speak(line) {
+//       console.log(`The ${this.type} rabbit says '${line}'`);
+//     }
+//   };
+//   let blackRabbit = Object.create(protoRabbit);
+//   blackRabbit.type = "black";
+//   blackRabbit.speak("I am fear and darkness");
+
+//   function makeRabbit(type) {
+//     let rabbit = Object.create(protoRabbit);
+//     rabbit.type = type;
+//     return rabbit;
+//   }
+//  makeRabbit("white")
+
+class Rabbit {
+  constructor(type) {
+    this.type = type;
+  }
+  speak(line) {
+    console.log(`The ${this.type} rabbit says '${line}'`);
+  }
+}
+
+let killerRabbit = new Rabbit("killer");
+console.log(Object.getPrototypeOf(killerRabbit))
